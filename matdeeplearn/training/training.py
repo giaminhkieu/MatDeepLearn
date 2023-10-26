9cbbda62ac4c602f5e26d4dbe4a54614e33e374c
giaminhkieu <67830212+giaminhkieu@users.noreply.github.com>
2023-10-26 15:45:40 +0800

    Update training.py

--- matdeeplearn/training/training.py
+++ matdeeplearn/training/training.py
@@ -990,8 +990,8 @@ def tune_setup(
     import ray
     from ray import tune
     from ray.tune.schedulers import ASHAScheduler
-    from ray.tune.suggest.hyperopt import HyperOptSearch
-    from ray.tune.suggest import ConcurrencyLimiter
+    from ray.tune.search.hyperopt import HyperOptSearch
+    from ray.tune.search import ConcurrencyLimiter
     from ray.tune import CLIReporter
 
     ray.init()
